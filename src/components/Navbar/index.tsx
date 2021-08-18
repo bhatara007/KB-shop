5e6c9ad62473518b74a597f7516244d5fcce4b95
bhatara007 <bhatara007@gmail.com>
2021-08-19 02:15:09 +0700

    add Nav and Banner

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-    return (
-        <div className='h-40 bg-transparent flex justify-between m-6'>
-            <div className='pl-7'>
+    return ( <>
+        <div className='h-40 bg-transparent flex justify-between m-6 
+        '>
+            <div className='pl-16 bg-transparent z-10'>
                 <p> LOGO </p>
             </div>
-            <div className='pr-5 space-x-6'>
+            <div className='pr-16 space-x-6 z-10'>
                 <Link href='/'>
                     Product
                 </Link>
@@ -22,6 +23,10 @@ const Navbar = () => {
                 </Link>
             </div>
         </div>
+        <div className='h-48 inset-0 absolute' style={{backgroundImage: "url(https://preview.redd.it/ycse9u77vm461.jpg?width=960&crop=smart&auto=webp&s=bbb3b9d9de8b20dd590fb05adedef10c1d5c5873)"}}>
+
+        </div>
+        </>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Banner from '../components/Banner/index,'
 
 
 export default function Home({ allPostsData }) {
