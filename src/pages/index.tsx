be6cd4e088c0f494cd50d4e4fd1e9cc3a197cdae
JAmoMES <atipat.pa@ku.th>
2021-08-19 02:38:40 +0700

    add prettier eslint husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import React from 'react'
-import Navbar from '../components/Navbar'
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
 
+import Navbar from '../components/Navbar';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home({ allPostsData }) {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
     </div>
-  )
+  );
 }
