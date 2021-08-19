d24c0d8ddf6e79be0d495f13f1d1776343a67ad4
JAmoMES <atipat.pa@ku.th>
2021-08-19 18:37:42 +0700

    add example fetch in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,45 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import React from 'react'
+import { GetStaticProps, InferGetServerSidePropsType, NextPage } from 'next'
+import React, { ReactElement } from 'react'
 
-import Banner from '../components/Banner/index,'
 import Navbar from '../components/Navbar'
-import utilStyles from '../styles/utils.module.css'
 
-export default function Home({ allPostsData }) {
+export interface HomeProps {
+  data: {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+  }[]
+}
+
+export const Home: NextPage<HomeProps> = ({ data }) => {
   return (
     <div>
       <Navbar />
+      <ul>
+        {data &&
+          data.map(item => {
+            return (
+              <li key={item.id}>
+                <h1>{item.title}</h1>
+                <p>{item.price}</p>
+                <p>{item.description}</p>
+                <p>{item.category}</p>
+                <img src={item.image} alt="this is image" />
+              </li>
+            )
+          })}
+      </ul>
     </div>
   )
+}
+
+export default Home
+
+Home.getInitialProps = async () => {
+  const res = await fetch('https://fakestoreapi.com/products')
+  const json = await res.json()
+  return { data: json }
 }
